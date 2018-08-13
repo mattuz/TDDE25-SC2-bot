bbceff393cc44e62795429d866765c3d79a60111
David Bergström <david-bergstrom@outlook.com>
2018-08-13 10:21:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# StarCraft II bot
+
+## Setup for creating your own bot
+
+Start of by forking this repository by pressing the fork button above, this
+will create your own copy of this repository.
+
+This will bring you to your own version of this page.
+
+Next you want to clone the new repository using the following command: `git
+clone <path>`, where `<path>` is the url to the right of the fork button you
+just pressed. If you have configured ssh-access use the ssh link, otherwise use
+the http link.
+
+Next, download the library binary from this page: (link)
+
+Put the library in the folder git repository you just downloaded.
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+
+from library import *
+
+
+class MyAgent(IDABot):
+    def __init__(self):
+        IDABot.__init__(self)
+
+    def on_game_start(self):
+        IDABot.on_game_start(self)
+
+    def on_step(self):
+        IDABot.on_step(self)
+
+
+def main():
+    coordinator = Coordinator()
+    bot1 = MyAgent()
+    # bot2 = MyAgent()
+
+    participant_1 = create_participants(Race.Terran, bot1)
+    # participant_2 = create_participants(Race.Terran, bot2)
+    participant_2 = create_computer(Race.Random, Difficulty.Easy)
+
+    coordinator.set_participants([participant_1, participant_2])
+
+    coordinator.launch_starcraft()
+
+    path = os.path.join(os.getcwd(), "maps", "InterloperTest.SC2Map")
+    coordinator.start_game(path)
+
+    while coordinator.update():
+        pass
+
+
+if __name__ == "__main__":
+    main()

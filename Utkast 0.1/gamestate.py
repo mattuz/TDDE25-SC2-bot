ea1a9369e8a98c9dea690e2bdad2f2c10bbd4609
Erik Boman <eribo549@student.liu.se>
2019-10-25 15:18:34 +0200

    saves current game values and units for later use

---
+++ Utkast 0.1/gamestate.py
@@ -0,0 +1,11 @@
+class gamestate:
+    """Data-class for MyAgent, more features to be added"""
+
+    AGENTUNITS = {}
+
+    MINERAL_WORKER = []
+    GAS_WORKER = []
+
+    def __init__(self):
+        pass
+
